f604694d29d0e99645c936f835b06c61aec3c197
emran-binhasan <emranbinhasan.zero@gmail.com>
2025-04-03 09:02:27 +0600

    add menu & upcoming event section

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './index.html',  // Adjust the paths as necessary
+      './src/**/*.{js,jsx,ts,tsx}', // Adjust for your file locations
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
